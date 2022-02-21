b8c3197fad4741d85af52aa23da278ecf3553a10
Kevin Pardo <contact@kevinpardo.com>
2022-02-21 11:41:25 +0100

    Hide pro login form

--- assets/js/mcf-login.js
+++ assets/js/mcf-login.js
@@ -1,17 +1,25 @@
-jQuery(document).ready(function($){
-    $('.js-reveal-login-form').click(function(e){
-        e.preventDefault()
-        if (!$(this).hasClass('on')) {
-            $('.mcf-connexion-block .item a').removeClass('on')
-            $(this).addClass('on')
-            $('.mcf-login-form, .mcf-form-success, .mcf-form-error').removeClass('visible')
-            $('#' + $(this).attr('data-form-id')).addClass('visible')
-        }
-    })
+jQuery(document).ready(function ($) {
+  $(".js-reveal-login-form").click(function (e) {
+    e.preventDefault();
+    if (!$(this).hasClass("on")) {
+      $(".mcf-connexion-block .item a").removeClass("on");
+      $(this).addClass("on");
+      $(".mcf-login-form, .mcf-form-success, .mcf-form-error").removeClass(
+        "visible"
+      );
+      $("#" + $(this).attr("data-form-id")).addClass("visible");
+    }
+  });
 
-    $('.js-recovery-password-opener').click(function(e){
-        e.preventDefault()
-        $('.mcf-recovery-form, .mcf-login-form, .mcf-form-success, .mcf-form-error').removeClass('visible')
-        $('#' + $(this).attr('data-recovery-form-id') + '-password').addClass('visible')
-    })
-})
+  $("#customer").addClass("visible");
+
+  $(".js-recovery-password-opener").click(function (e) {
+    e.preventDefault();
+    $(
+      ".mcf-recovery-form, .mcf-login-form, .mcf-form-success, .mcf-form-error"
+    ).removeClass("visible");
+    $("#" + $(this).attr("data-recovery-form-id") + "-password").addClass(
+      "visible"
+    );
+  });
+});
--- templates/mcf-login-block.php
+++ templates/mcf-login-block.php
@@ -2,31 +2,12 @@
     <div class="small-12 column">
         <div class="mcf-connexion-block js-mcf-connexion-block">
             <?php if (!empty($mcf_options['main_title'])) : ?>
-                <h3 class="block-title"><?php print $mcf_options['main_title']; ?></h3>
+            <h3 class="block-title"><?php print $mcf_options['main_title']; ?></h3>
             <?php endif; ?>
 
             <?php if (!empty($mcf_options['explanations'])) : ?>
-                <p><?php print $mcf_options['explanations']; ?></p>
+            <p><?php print $mcf_options['explanations']; ?></p>
             <?php endif; ?>
-
-            <h4 class="block-subtitle"><?php _e('Login as', 'mcf_login'); ?></h4>
-
-            <ul>
-                <li class="item">
-                    <a href="#" class="js-reveal-login-form<?php print ((!empty($_GET['errorcount']) || !empty($_GET['successpasswordrecovery'])) && !empty($_GET['mcfform']) && 'provider' == $_GET['mcfform']) ? ' on' : ''; ?>" data-form-id="provider">
-                        <span class="dashicons dashicons-businessman"></span>
-                        <span class="name"><?php _e('Provider', 'mcf_login'); ?></span>
-                    </a>
-                </li>
-
-                <li class="item">
-                    <a href="#" class="reveal-login-form-button js-reveal-login-form<?php print ((!empty($_GET['errorcount']) || !empty($_GET['successpasswordrecovery'])) && !empty($_GET['mcfform']) && 'customer' == $_GET['mcfform']) ? ' on' : ''; ?>" data-form-id="customer">
-                        <span class="dashicons dashicons-businessperson"></span>
-                        <span class="name"><?php _e('Customer', 'mcf_login'); ?></span>
-                    </a>
-                </li>
-            </ul>
-
             <?php
             // Forms
             foreach ($forms as $form_key => $form) :
@@ -42,10 +23,38 @@
 </div>
 
 <style>
-    .mcf-connexion-block { background-color : <?php print $mcf_options['background_color']; ?>!important; }
-    .mcf-connexion-block .block-title, .mcf-connexion-block p, .mcf-connexion-block .block-subtitle, .mcf-connexion-block ul .item a, .mcf-login-form .form-label { color : <?php print $mcf_options['main_color']; ?>!important; }
-    .mcf-login-form .form-input input:focus { border-color : <?php print $mcf_options['second_color']; ?>!important; }
-    .mcf-connexion-block ul .item a.on, .mcf-connexion-block ul .item a:hover { color : <?php print $mcf_options['second_color']; ?>!important; }
-    .mcf-login-form .form-group.submit .submit-button input[type="submit"] { background-color : <?php print $mcf_options['main_color']; ?>!important; }
-    .mcf-login-form .form-group.submit .submit-button input[type="submit"]:hover { background-color : <?php print $mcf_options['second_color']; ?>!important; }
+.mcf-connexion-block {
+    background-color: <?php print $mcf_options['background_color'];
+    ?> !important;
+}
+
+.mcf-connexion-block .block-title,
+.mcf-connexion-block p,
+.mcf-connexion-block .block-subtitle,
+.mcf-connexion-block ul .item a,
+.mcf-login-form .form-label {
+    color: <?php print $mcf_options['main_color'];
+    ?> !important;
+}
+
+.mcf-login-form .form-input input:focus {
+    border-color: <?php print $mcf_options['second_color'];
+    ?> !important;
+}
+
+.mcf-connexion-block ul .item a.on,
+.mcf-connexion-block ul .item a:hover {
+    color: <?php print $mcf_options['second_color'];
+    ?> !important;
+}
+
+.mcf-login-form .form-group.submit .submit-button input[type="submit"] {
+    background-color: <?php print $mcf_options['main_color'];
+    ?> !important;
+}
+
+.mcf-login-form .form-group.submit .submit-button input[type="submit"]:hover {
+    background-color: <?php print $mcf_options['second_color'];
+    ?> !important;
+}
 </style>
